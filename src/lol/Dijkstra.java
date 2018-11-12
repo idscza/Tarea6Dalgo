130ee1e89f98df76acb824495254373e1fed01b1
Ditto <id.salazar@uniandes.edu.co>
2018-11-11 21:27:11 -0500

    que mierda esto

--- src/lol/Dijkstra.java
+++ src/lol/Dijkstra.java
@@ -1,5 +1,17 @@
 package lol;
 
 public class Dijkstra {
+	
+	private int[][] caminosCortos;
+	private Grafito gg;
 
+	public Dijkstra(Grafito g) {
+		gg = g;
+		caminosCortos =  new int[g.getMatriz().length][g.getMatriz().length];
+		for (int i = 0; i < caminosCortos.length; i++) {
+			for (int j = 0; j < caminosCortos.length; j++) {
+				caminosCortos[i][j] = -1;
+			}
+		}
+	}
 }
--- src/lol/FW.java
+++ src/lol/FW.java
@@ -2,4 +2,16 @@ package lol;
 
 public class FW {
 
+	private int[][] caminosCortos;
+	private Grafito gg;
+
+	public FW(Grafito g) {
+		gg = g;
+		caminosCortos =  new int[g.getMatriz().length][g.getMatriz().length];
+		for (int i = 0; i < caminosCortos.length; i++) {
+			for (int j = 0; j < caminosCortos.length; j++) {
+				caminosCortos[i][j] = -1;
+			}
+		}
+	}
 }
